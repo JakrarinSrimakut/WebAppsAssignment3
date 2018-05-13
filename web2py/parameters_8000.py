428c519b0c089e8d7feb708e95e5f28199009255
Jack <gungravefire@yahoo.com>
2018-05-13 06:00:34 -0700

    log in visible memo

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a794a9a2dd454cd1$9f4ecde4099fcace84fa6cc1a6dfa48abad8c44f"
+password="pbkdf2(1000,20,sha512)$837566f372a1a3f7$369a51acbeec479c8b72978f5f7243669791d237"
